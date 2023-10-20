14c9ffd314cacfcfc10548f9c977efcd7a90101c
David De Anda <omarandstuff@gmail.com>
2023-10-19 21:46:56 -0600

    Jest config for CI and node 20

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,5 @@
-jest.retryTimes(10)
-jest.setTimeout(10000)
+// node > 19 has some issues with fetch closing sockets on consecutive requests
+if (process.env.CI || process.versions.node.startsWith('20')) {
+  jest.retryTimes(5)
+  jest.setTimeout(10000)
+}
