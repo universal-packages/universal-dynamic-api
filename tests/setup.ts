f4a30ee0f6e4e603807fce88b612faa4f82250b1
David De Anda <omarandstuff@gmail.com>
2024-03-24 11:49:23 -0600

    Default 10 retries in CI

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,5 @@
 // node > 19 has some issues with fetch closing sockets on consecutive requests
 if (process.env.CI || process.versions.node.startsWith('20')) {
-  jest.retryTimes(5)
+  jest.retryTimes(10)
   jest.setTimeout(10000)
 }
